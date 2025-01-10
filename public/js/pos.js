8f5972ed58d2775bb970f376da6f76b9461c7265
ADJIE HIMAWAN <himawantakarai@gmail.com>
2025-01-10 23:14:23 +0800

    total

--- public/js/pos.js
+++ public/js/pos.js
@@ -1175,7 +1175,8 @@ const apiDomain = "https://admin.sidikty.com/api";
 const posDomain = window.location.protocol + "//" + window.location.hostname;
 var transactionCode = null;
 var _token = document.querySelector('meta[name="csrf-token"]').getAttribute('content') || $('meta[name="csrf-token"]').attr('content');
-var namaUser, saldo, id_usercard, onPay;
+var namaUser, saldo, id_usercard;
+var total;
 
 async function isBarcodeValid(barcode) {
     $.ajax({
@@ -1195,11 +1196,10 @@ async function isBarcodeValid(barcode) {
                 namaUser = response.data.namaUser;
                 saldo = response.data.saldo;
                 id_usercard = response.data.userCardId;
-                onPay = document.getElementById('on_pay');
-
                 $('#namaUser').val(namaUser);
                 $('#saldo').val(saldo);
                 $('#id_usercard').val(id_usercard);
+                total = $('#jumlahtotal').val().replace(/\D/g, '');
 
                 let validasi = isValidasi(response.data.userCardId);
                 validasi.then((validasi) => {
@@ -1215,7 +1215,7 @@ async function isBarcodeValid(barcode) {
                             $('#saldo').val('');
                             $('#id_usercard').val('');
                         });
-                    if(validasi.sisa_limit < onPay && validasi.sisa_limit !== null){
+                    } else if(validasi.sisa_limit < total && validasi.sisa_limit !== null){
                         Swal.fire({
                             title: 'Error',
                             text: 'Limit harian belanja tidak cukup',
@@ -1227,7 +1227,7 @@ async function isBarcodeValid(barcode) {
                             $('#saldo').val('');
                             $('#id_usercard').val('');
                         });
-                    } else if(response.data.saldo < onPay){
+                    } else if(response.data.saldo < total){
                         Swal.fire({
                             title: 'Error',
                             text: 'Saldo tidak cukup',
