5d013333b4aad12e28b7374ff424c1590a09359b
ADJIE HIMAWAN <himawantakarai@gmail.com>
2025-01-18 03:23:20 +0800

    test

--- app/Imports/ProductSheet.php
+++ app/Imports/ProductSheet.php
@@ -7,30 +7,62 @@ use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\Validator;
 
 class ProductSheet implements ToModel, WithHeadingRow
 {
     /**
     * @param Collection $collection
     */
+    // public function model(array $row)
+    // {
+        
+    //     // Check if a product with the same id already exists
+    //     if (Product::where('id', $row['id'])->exists()) {
+    //         return null; // Skip this row
+    //     }
+
+    //     return new Product([
+    //         'id'            => $row['id'],
+    //         'name'          => $row['product_name'],
+    //         'sku'           => $row['sku'],
+    //         'barcode_type'  => $row['barcode_type'],
+    //         'category_id'   => $row['category_id'],
+    //         'brand_id'      => $row['brand_id'],
+    //         'unit_id'       => $row['unit_id'],
+    //         'alert_quantity'    => $row['alert_qty'],
+    //         'type'          => $row['product_type']
+    //     ]);
+    // }
+
+
     public function model(array $row)
     {
-        
-        // Check if a product with the same id already exists
-        if (Product::where('id', $row['id'])->exists()) {
-            return null; // Skip this row
-        }
+        if ($row['id'] != null) {
+            if ($row['product_name'] == null || $row['sku'] == null || $row['barcode_type'] == null || $row['category_id'] == null || $row['alert_qty'] == null  || $row['product_type'] == null) {
+                Validator::make($row, [
+                    'id'        => 'required|unique:products,id',
+                    'product_name'    => 'required',
+                    'sku'    => 'required',
+                    'barcode_type'    => 'required',
+                    'category_id'    => 'required',
+                    'alert_qty'    => 'required',
+                    'product_type'    => 'required'
+                ])->validate();
+            }
 
-        return new Product([
-            'id'            => $row['id'],
-            'name'          => $row['product_name'],
-            'sku'           => $row['sku'],
-            'barcode_type'  => $row['barcode_type'],
-            'category_id'   => $row['category_id'],
-            'brand_id'      => $row['brand_id'],
-            'unit_id'       => $row['unit_id'],
-            'alert_quantity'    => $row['alert_qty'],
-            'type'          => $row['product_type']
-        ]);
+            $brand = $row['brand_id'] ? $row['brand_id'] : null;
+
+            return new Product([
+                'id'            => $row['id'],
+                'name'          => $row['product_name'],
+                'sku'           => $row['sku'],
+                'barcode_type'  => $row['barcode_type'],
+                'category_id'   => $row['category_id'],
+                'brand_id'      => $brand,
+                'alert_quantity'    => $row['alert_qty'],
+                'type'          => $row['product_type']
+            ]);
+        }
     }
 }
--- app/Imports/VariantSheet.php
+++ app/Imports/VariantSheet.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\Validator;
 
 class VariantSheet implements ToModel, WithHeadingRow
 {
@@ -15,37 +16,104 @@ class VariantSheet implements ToModel, WithHeadingRow
     * @param Collection $collection
     */
     
-    public function model(array $row)
-    {
-        $margin = ($row['selling_price'] / $row['purchase_price']) * 100 - 100;
-        $getMargin = ceil($margin);
+    // public function model(array $row)
+    // {
+    //     $margin = ($row['selling_price'] / $row['purchase_price']) * 100 - 100;
+    //     $getMargin = ceil($margin);
 
-        $product = Product::where('id', $row['product_id'])->whereNotNull('deleted_at')->first();
-        $product_exist = Product::where('id', $row['product_id'])->exists();
-        if ($product) {
-            return null;
-        }
+    //     $product = Product::where('id', $row['product_id'])->whereNotNull('deleted_at')->first();
+    //     $product_exist = Product::where('id', $row['product_id'])->exists();
+    //     if ($product) {
+    //         return null;
+    //     }
 
-        if (!$product_exist) {
-            return null;
-        }
+    //     if (!$product_exist) {
+    //         return null;
+    //     }
         
-        return new Variation(
+    //     return new Variation(
            
-                [
-                // 'id'        => $row['id'],
-                'product_id'    => $row['product_id'],
-                'sku'       => $row['sku'],
-                'price_inc_tax' => $row['purchase_price'],
-                'purchase_price'    => $row['purchase_price'],
-                'name'      => $row['name'],
-                'selling_price' => $row['selling_price'],
-                'margin'    => $getMargin
-            ]
-        );
+    //             [
+    //             // 'id'        => $row['id'],
+    //             'product_id'    => $row['product_id'],
+    //             'sku'       => $row['sku'],
+    //             'price_inc_tax' => $row['purchase_price'],
+    //             'purchase_price'    => $row['purchase_price'],
+    //             'name'      => $row['name'],
+    //             'selling_price' => $row['selling_price'],
+    //             'margin'    => $getMargin
+    //         ]
+    //     );
         
 
 
        
+    // }
+
+
+    public function model(array $row)
+    {
+
+        if ($row['id'] != null) {
+            if (
+                $row['product_id'] == null  || $row['purchase_price'] == null || $row['name'] == null || $row['selling_price'] == null
+                || $row['unit_id'] == null
+            ) {
+                Validator::make($row, [
+                    'id'        => 'required|unique:variations,id',
+                    'product_id'      => 'required',
+                    'purchase_price'   => 'required',
+                    'name'   => 'required',
+                    'selling_price' => 'required',
+                    'unit_id'   => 'required'
+                ])->validate();
+            }
+
+            if ($row['selling_price'] > 0 && $row['purchase_price'] > 0) {
+
+                $margin = ($row['selling_price'] / $row['purchase_price']) * 100 - 100;
+                $getMargin = ceil($margin);
+                $sku = $row['sku'] ? $row['sku'] : $this->generateEAN();
+                // $rak = $row['rak_id'] ? $row['rak_id'] : null;
+                // $tax = $row['tax'] ? $row['tax'] : 0;
+
+                return new Variation(
+                    [
+                        'id'        => $row['id'],
+                        'product_id'    => $row['product_id'],
+                        'sku'       => $sku,
+                        'purchase_price'    => $row['purchase_price'],
+                        'name'      => $row['name'],
+                        'selling_price' => $row['selling_price'],
+                        'margin'    => $getMargin,
+                        'unit_id'   => $row['unit_id'],
+                    ]
+                );
+            }
+        }
+    }
+
+    function generateEAN()
+    {
+        $code = '200' . str_pad($this->generateRandomCode(), 9, '0');
+        $weightflag = true;
+        $sum = 0;
+        for ($i = strlen($code) - 1; $i >= 0; $i--) {
+            $sum += (int)$code[$i] * ($weightflag ? 3 : 1);
+            $weightflag = !$weightflag;
+        }
+        $code .= (10 - ($sum % 10)) % 10;
+        return $code;
+    }
+
+    function generateRandomCode($length = 8)
+    {
+        $characters = '0123456789';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
