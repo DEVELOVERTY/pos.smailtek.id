9826ed964183ea625d98d961b6bd4db254f9c4a0
Syahrul Hidayat <syahrulhidayat320@gmail.com>
2025-09-25 17:05:50 +0800

    kembalikan ke settingan sandbox

--- config/kedit.php
+++ config/kedit.php
@@ -8,10 +8,10 @@ return [
     |
     */
     // Testing
-    'base_url' => env('KEDIT_BASE_URL', 'http://127.0.0.1:8000'),
-    'pos_base_url' => env('POS_BASE_URL', 'http://127.0.0.1:8001'),
+    // 'base_url' => env('KEDIT_BASE_URL', 'http://127.0.0.1:8000'),
+    // 'pos_base_url' => env('POS_BASE_URL', 'http://127.0.0.1:8001'),
 
     // Production
-    // 'base_url' => env('KEDIT_BASE_URL', 'https://sandbox-sidik.smailtek.id'),
-    // 'pos_base_url' => env('POS_BASE_URL', 'https://sandbox-pos.smailtek.id'),
+    'base_url' => env('KEDIT_BASE_URL', 'https://sandbox-sidik.smailtek.id'),
+    'pos_base_url' => env('POS_BASE_URL', 'https://sandbox-pos.smailtek.id'),
 ];
